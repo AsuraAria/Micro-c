c6bbebd477a99b91f1e7f1f4771e1c25d56478b4
dixvies <divi.eflamm@gmail.com>
2019-03-27 16:44:34 +0100

    update musique, a tester(fonctionne normalement)
    De la doc et des prototypes on ete rajouté.
    Tester si ca compile bien
    pas de modification en profondeur

--- User/musicien.c
+++ User/musicien.c
@@ -4,7 +4,7 @@
 #include "lpc17xx.h"
 #include "partitions.h"
 
-short nbperi;//nb periodes de note
+short nbperi;//nb periodes de la note en cours
 short frontmusique;//pointe vers la note qui doit etre joué
 LPC_TIM_TypeDef val_reg_timer;//variable globale
 
@@ -17,7 +17,8 @@ tnote * addr_note_actuelle =0;//pointe vers la tnote que l'on est en train de jo
 tpartition pactuelle;//indique la partition que l'on est en train de jouer
 char on_repeat =0;//indique si la partition actuelle est en mode repeat
 
-tnote nsilence;//variable temporaire lorsque la note est silencieuxse
+tnote nsilence;//variable temporaire indiquant lorsque la note est silencieuxse
+
 
 void jouer_note(tnote note){//parametre les variables globales pour que le handler joue note
 	//Freq divisé par 2
@@ -27,9 +28,10 @@ void jouer_note(tnote note){//parametre les variables globales pour que le handl
 	Matchrestant= MatchValue;
 }
 
+
 void jouer_partition(tpartition partition){
   if(partition[0].longueur != 0){//on joue la note si cest pas la fin
-		if(partition[0].frequence != 0){//si la note nest pas silencieuxse
+		if(partition[0].frequence != 0){//si la note nest pas silencieuse
 			silencieux = 0;
 			addr_note_actuelle = partition;
 			jouer_note(*partition);
@@ -45,15 +47,6 @@ void jouer_partition(tpartition partition){
 	}
 }
 
-/*void sleep(int s)//ancienne fonction d'attente en ms
-{
-	int i=0;
-
-	while(i<s*100)
-	{
-		i++;
-	}
-}*/
 
 void T0_Init(){//initiation du timer0
 
@@ -62,26 +55,27 @@ void T0_Init(){//initiation du timer0
 
 
 	conf_timer.PrescaleOption = TIM_PRESCALE_USVAL; //prescale en uS
-	conf_timer.PrescaleValue = 1;//precision de 50 uS
+	conf_timer.PrescaleValue = 1;//precision de 1 uS
 
 	conf_interrupt.MatchChannel=0;//timer0
 	conf_interrupt.IntOnMatch=ENABLE;//enable:interruption sur match
 	conf_interrupt.ResetOnMatch=ENABLE;//remise a zero en match
-	conf_interrupt.StopOnMatch = DISABLE;
-	conf_interrupt.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
-	conf_interrupt.MatchValue=50;
+	conf_interrupt.StopOnMatch = DISABLE;//le timer ne s'arrete pas lors d'un match
+	conf_interrupt.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;//aucune sortie lors du match
+	conf_interrupt.MatchValue=50;//on attend 50 fois 1us
 
-	NVIC_EnableIRQ(TIMER0_IRQn);
+	NVIC_EnableIRQ(TIMER0_IRQn);//activation du handler
 
 	TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &conf_timer);
   //timer0 en rising avec la config ci-jointe
 
-	TIM_ConfigMatch(LPC_TIM0, &conf_interrupt);
+	TIM_ConfigMatch(LPC_TIM0, &conf_interrupt);//configuration du match
 
-	TIM_Cmd(LPC_TIM0, ENABLE);
-	//*/
+	TIM_Cmd(LPC_TIM0, ENABLE);//lancement du timer
+
 	MatchValue = -1;//on commence sans jouer de musique
 }
+
 
 void TIMER0_IRQHandler(){//change tous les timers temps le front
 
@@ -100,10 +94,10 @@ void TIMER0_IRQHandler(){//change tous les timers temps le front
 			nbperi = nbperi -1;
 		}else if (addr_note_actuelle!=0){
 				jouer_partition(&(addr_note_actuelle[1]));
-			
+
 		}
 	}
-	
+
 	if(Matchrestant != -1){//on decremente le nombre de match avant le prochain front sonor
 		Matchrestant = Matchrestant -1;
 	}
@@ -115,20 +109,24 @@ void TIMER0_IRQHandler(){//change tous les timers temps le front
 	TIM_ClearIntPending(LPC_TIM0, TIM_MR0_INT);//Acquitement
 }
 
+
 void musintro(void){//joue la musique d'intro
 		jouer_en_repeat(thprincipal);
 }
+
 
 void jouer_en_repeat(tpartition partition){//joue la partition designe en mode repeat
 	on_repeat=1;
 	pactuelle = partition;
 	jouer_partition(partition);
-  		
+
 }
+
 
 void arreter_repeat(){//arrete le repeat du morceau
 	on_repeat=0;
 }
+
 
 void jouer_1_fois(tpartition partition){//joue une seule fois la partition
 	on_repeat=0;
--- User/musicien.h
+++ User/musicien.h
@@ -8,42 +8,101 @@ typedef struct
 //une partition est un tableaux de notes se terminant par une note de longueur 0
 typedef tnote * tpartition;
 
-//note de fin
+//note de fin de partition
 #define finp {42,0}
 
 //notes de musique
-
-//Frequence divisé par 2
-
+//Frequence divise par 2
 #define do3 261/2
-
 #define re3 293/2
-
 #define mi3 330/2
-
 #define fa3 350/2
-
 #define sol2 196/2
-
-#define sol3 392/2 //730 //784 en vrai
-
+#define sol3 392/2
 #define la2 220/2
 
 
-#define usil {0,3} //petit silence de coupure
+#define usil {0,3} //petit silence de coupure entre deux notes identiques
 
 //joue une note
 void jouer_note(tnote);
-
+/*********************************************************************//**
+ * @brief			configure les variables utilisees par le handler pour jouer une note
+ * @param[in]	tnote
+ * @return		None
+ *
+ * Note:
+ * n'attend pas que la precedente note soit finie
+ * pour commencer la nouvelle
+ **********************************************************************/
 void jouer_partition(tpartition);
+/*********************************************************************//**
+ * @brief			procedure intermediaire joue une tpartition
+ * @param[in]	tpartition
+ * @return		None
+ *
+ * Note:
+ * arrete de jouer a la fin de la partition
+ * ne fais plus rien ensuite
+ **********************************************************************/
 
 void T0_Init();
+/*********************************************************************//**
+ * @brief			initialise le timer0 en mode interruption
+ * @param[in]	None
+ * @return		None
+ *
+ * Note:
+ * Interruptions toutes les 50microsecondes
+ **********************************************************************/
 
 void TIMER0_IRQHandler();
-
-void n(int t);
+/*********************************************************************//**
+ * @brief			Handler des interruptions du timer0
+ * @param[in]	None
+ * @return		None
+ *
+ * Note:
+ * -s'occuppe directement de la manipulation du buzzer
+ * -utilise des variables globales pour fonctionner
+ **********************************************************************/
 
 void musintro();
+/*********************************************************************//**
+ * @brief			joue le son lors du demarrage
+ * @param[in]	None
+ * @return		None
+ *
+ * Note:
+ * joue le theme principal en boucle
+ **********************************************************************/
 
 void jouer_en_repeat(tpartition p);
- //void n2();
+/*********************************************************************//**
+ * @brief			joue une tpartition en repeat
+ * @param[in]	None
+ * @return		None
+ *
+ * Note:
+ * le fonctionnement est le meme que jouer_partition()
+ * mais en repeat
+ **********************************************************************/
+void arreter_repeat();
+/*********************************************************************//**
+ * @brief			stope le mode repeat
+ * @param[in]	None
+ * @return		None
+ *
+ * Note:
+ * La partition en cours continuera jusqu'a la fin
+ **********************************************************************/
+
+ void jouer_1_fois(tpartition);
+ /*********************************************************************//**
+  * @brief			joue une partition une seule fois
+  * @param[in]	None
+  * @return		None
+  *
+  * Note:
+  * a utiliser a la place de jouer_partition()
+  **********************************************************************/
