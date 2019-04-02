38c27730c6083db368e6abb3e4fbeb24b7fca6e3
Elex <elex17@hotmail.fr>
2019-04-02 10:40:00 +0200

    Revert "Alpha V0.1"
    This reverts commit 8689dc9ee5e3c5a9a3fbdca7debca9fc827412ab.

--- User/main.c
+++ User/main.c
@@ -1,9 +1,9 @@
 //===========================================================//
 // Projet Micro - INFO1 - ENSSAT - S2 2018							 //
 //===========================================================//
-// File                : Programme de depart
+// File                : Programme de d�part
 // Hardware Environment: Open1768
-// Build Environment   : Keil Vision
+// Build Environment   : Keil �Vision
 //===========================================================//
 
 #include "lpc17xx_gpio.h"
@@ -19,26 +19,20 @@
 #include "memory.h"
 #include "musicien.h"
 
-// fichier contenant toutes les declarations de variables globales
-#include "globaldec.h"
+#include "globaldec.h" // fichier contenant toutes les d�clarations de variables globales
 #include <stdio.h>
 
-// initiation du timer1
-void T1_Init(){
+void T1_Init(){//initiation du timer1
 
 	TIM_TIMERCFG_Type conf_timer;
 	TIM_MATCHCFG_Type conf_interrupt;
 
-	// prescale en mS
-	conf_timer.PrescaleOption = TIM_PRESCALE_USVAL;
+	conf_timer.PrescaleOption = TIM_PRESCALE_USVAL; //prescale en mS
 	conf_timer.PrescaleValue = 100000;
 
-	// timer1
-	conf_interrupt.MatchChannel=1;
-	// enable:interruption sur match
-	conf_interrupt.IntOnMatch=ENABLE;
-	// remise a zero en match
-	conf_interrupt.ResetOnMatch=ENABLE;
+	conf_interrupt.MatchChannel=1;//timer1
+	conf_interrupt.IntOnMatch=ENABLE;//enable:interruption sur match
+	conf_interrupt.ResetOnMatch=ENABLE;//remise a zero en match
 	conf_interrupt.StopOnMatch = DISABLE;
 	conf_interrupt.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
 	conf_interrupt.MatchValue=1;
@@ -54,7 +48,7 @@ void T1_Init(){
 }
 
 void TIMER1_IRQHandler()
-{
+{//change tous les timers temps le front
 	if (!(GPIO_ReadValue(0) & (1<<19)))
 	{
 		flagTouch = 1;
@@ -63,74 +57,63 @@ void TIMER1_IRQHandler()
 	{
 		flagReset = 1;
 	}
-
+	
 	if (hitReset<hitResetMax)
 	{
 		hitReset++;
 	}
-
+	
 	enFlag = 1;
-
+	
 	if (stamina < staminaMax)
 	{
 		stamina++;
 	}
-	// Acquitement
-	TIM_ClearIntPending(LPC_TIM1, TIM_MR1_INT);
+	
+	TIM_ClearIntPending(LPC_TIM1, TIM_MR1_INT);//Acquitement
 }
 
 void pin_Configuration()
 {
 		unsigned char i;
-
+	
 		PINSEL_CFG_Type memory27;
 		PINSEL_CFG_Type memory28;
 
 		int ledBit = (1<<3);
 		int hpBit = (1<<9);
- 		// Port
-		int joystickPort[6] = {2, 2, 2, 2, 2, 1};
-		// Pin
-		int joystickPin[6] = {10, 12, 13, 8, 11, 21};
+	
+		int joystickPort[6] = {2, 2, 2, 2, 2, 1}; //Port
+		int joystickPin[6] = {10, 12, 13, 8, 11, 21}; //Pin
+	
+		FIO_SetDir(2, (1<<11), 0); // Bouton Menu : Entrée
+		FIO_SetMask(2, (1<<11), 0); // Bouton Menu : Utilisable
+		
+		FIO_SetDir(0, ledBit, 1); // Led0 : Sortie
+		FIO_SetMask(0, ledBit, 0); // Led0 : Utilisable
 
-		// Bouton Menu : Entrée
-		FIO_SetDir(2, (1<<11), 0);
-		// Bouton Menu : Utilisable
-		FIO_SetMask(2, (1<<11), 0);
-
-		// Led0 : Sortie
-		FIO_SetDir(0, ledBit, 1);
-		// Led0 : Utilisable
-		FIO_SetMask(0, ledBit, 0);
-
-		// Haut-Parleur : Sortie
-		FIO_SetDir(1, hpBit, 1);
-		// Haut-Patleut : Utilisable
-		FIO_SetMask(1, hpBit, 0);
+		FIO_SetDir(1, hpBit, 1); // Haut-Parleur : Sortie
+		FIO_SetMask(1, hpBit, 0); // Haut-Patleut : Utilisable
 
 		memory27.Funcnum = PINSEL_FUNC_1;
 		memory27.OpenDrain = PINSEL_PINMODE_NORMAL;
 		memory27.Pinmode = PINSEL_PINMODE_PULLUP;
 		memory27.Pinnum = PINSEL_PIN_27;
-		// GPIO0
-		memory27.Portnum = PINSEL_PORT_0;
+		memory27.Portnum = PINSEL_PORT_0; // GPIO0
 		PINSEL_ConfigPin(&memory27);
 
 		memory28.Funcnum = PINSEL_FUNC_1;
 		memory28.OpenDrain = PINSEL_PINMODE_NORMAL;
 		memory28.Pinmode = PINSEL_PINMODE_PULLUP;
 		memory28.Pinnum = PINSEL_PIN_28;
-		// GPIO0
-		memory28.Portnum = PINSEL_PORT_0;
+		memory28.Portnum = PINSEL_PORT_0; // GPIO0
 		PINSEL_ConfigPin(&memory28);
-
+		
 		// Congifuration
 		for (i=0; i<6; i++)
 		{
-			// joystick : Entrée
-			FIO_SetDir(joystickPort[i], joystickPin[i], 0);
-			// joystick : Utilisable
-			FIO_SetMask(joystickPort[i], joystickPin[i], 0);
+			FIO_SetDir(joystickPort[i], joystickPin[i], 0); // joystick : Entrée
+			FIO_SetMask(joystickPort[i], joystickPin[i], 0); // joystick : Utilisable
 		}
 }
 
@@ -152,12 +135,12 @@ int main(void)
 	// Definition
 	//=============
 	char i;
-
+	
 	// Variable pour savoir si un tableau doit être affiché
 	bool mapLoad = true;
 	// Variables des coordonnées du tableau à afficher
 	unsigned char mapX=0, mapY=0;
-
+	
 	// Coordonnées du joueur et direction pour afficher le sprite
 	unsigned short pX=30, pY=30;
 	signed char dir=4, vit=1;
@@ -165,38 +148,32 @@ int main(void)
 	unsigned char pLife = 0;
 	// Coordonnées de l'ancienne position du joueur
 	unsigned short pBX=pX, pBY=pY;
-
+	
 	signed char menu = 1;
-
+	
 	char numSave = 0;
-
+	
 	//Les enemies bougent 2 fois moins vite
 	char iEn=0;
-
+	
 	//uint16_t previousTouch = 0;
-
+	
   	uint8_t data[20];
 	//=============
 	// Init
 	//=============
-
-	// init pinsel ecran et init LCD
-	lcd_Initializtion();
-	// init pinsel tactile et init tactile
-	touch_init();
-	// init pinsel memoire, buzzer, led
-	pin_Configuration();
-	// init mémoire
-	init_i2c_eeprom();
-	// init timer musique
-	T0_Init();
+	
+	lcd_Initializtion(); // init pinsel ecran et init LCD
+	touch_init(); // init pinsel tactile et init tactile
+	pin_Configuration(); // init pinsel memoire, buzzer, led
+	init_i2c_eeprom(); // init mémoire
+	T0_Init(); // init timer musique
 	T1_Init();
-
+	
 	musintro();
-
-	if (!check_memory() || !(GPIO_ReadValue(2) & (1<<11)))
+	
+	if (!check_memory() || !(GPIO_ReadValue(2) & (1<<11))) // (Si la sauvegarde 0 existe : 
 	{
- 		// Si la memoire est invalide ou on appuie sur la réinitialisation
 		clean_memory();
 		create_gamekey();
 	}
@@ -204,27 +181,26 @@ int main(void)
 	//=============
 	// Boucle
 	//=============
-
+	
 	dessiner_rect(0,0,240,320,0,1,Black,Black);
-
+	
 	while(1)
 	{
 
-
+		
 		if (menu == -1)
 		{
 			// Si il y a un changement de tableau, on affiche ce tableau (x,y) sur l'écran
 			if (mapLoad)
 			{
 				drawMap(mapX, mapY, &mapLoad);
-				// On le fait qu'une seule fois pour une question de rapidité d'execution
-				pLife = 0;
+				pLife = 0;				// On le fait qu'une seule fois pour une question de rapidité d'execution
 			}
-
+			
 			if (stamina != staminaMax)
 				dessiner_rect(8,10, 4, staminaMax-stamina, 0, 1, Grey, Grey);
 			dessiner_rect(8,10+staminaMax-stamina, 4, stamina, 0, 1, Blue, Blue);
-
+			
 			if (pLife != life)
 			{
 				for (i=0; i<3; i++)
@@ -240,13 +216,13 @@ int main(void)
 				}
 				pLife = life;
 			}
-
+			
 			// Rafraîchir les cases derrière l'ancienne position du personnage pour le faire disparaître
 			if (pBX!=pX || pBY != pY)
 			{
 				clearOldPlayer(pBX, pBY, mapX, mapY);
 			}
-
+			
 			if (iEn==0)
 			{
 				if (mapX<250 && (mapX > 0 || mapY > 0))
@@ -257,35 +233,35 @@ int main(void)
 						{
 							clearOldPlayer(peX[i], peY[i], mapX, mapY);
 							oneMoreClear[i]=0;
-						}
+						}	
 						peX[i] = eX[i], peY[i] = eY[i];
 					}
 				}
 			}
-
+			
 			// Raffraichir la zone d'effaçage
 			pBX=pX, pBY=pY;
-
+			
 			// Gestion du joystick
 			dir = readJoystick();
-
+			
 			// Retourner au menu
 			if (dir == 5)
 			{
 				menu = 1;
 				mapLoad = true;
-				// Préparation de la sauvegarde "data"
-				fillup_save(data, numSave, life, mapX, mapY, pX, pY, 0);
+				
+				fillup_save(data, numSave, life, mapX, mapY, pX, pY, 0); // Préparation de la sauvegarde "data"
 				create_save(numSave,data);
 			}
-
+			
 			// Attaquer
 			if (dir == 6 && stamina >= staminaAttack)
 			{
 				attack(pX, pY, eX, eY, oneMoreClear, &numEn);
 				stamina -= staminaAttack;
 			}
-
+			
 			// Déplacement du personnage
 			if (!isColliding(pX, pY, mapX, mapY, dir))
 			{
@@ -310,7 +286,7 @@ int main(void)
 			{
 				dir = 4;
 			}
-
+			
 			// Changement de tableau
 			if (pX >= 220)
 			{
@@ -341,10 +317,10 @@ int main(void)
 				numEn = 3;
 				initEnemy(eX, eY, random);
 			}
-
+			
 			// Affiche le joueur sur l'écran
 			drawPlayer(pX, pY, dir);//dir
-
+			
 			if (iEn==0)
 			{
 				if (mapX<250 && (mapX > 0 || mapY > 0))
@@ -366,13 +342,13 @@ int main(void)
 						pY=30;
 						mapX=0;
 						mapY=0;
-						// Préparation de la sauvegarde "data"
-						fillup_save(data, numSave, life, mapX, mapY, pX, pY, 0);
+						
+						fillup_save(data, numSave, life, mapX, mapY, pX, pY, 0); // Préparation de la sauvegarde "data"
 						create_save(numSave,data);
 					}
-
+					
 					vit = (signed char)numEn;
-
+					
 					for (i=0; i<6; i++)
 					{
 						if (eX[i] > 0)
@@ -404,7 +380,7 @@ int main(void)
 								eY[i]>pY?eY[i]--:eY[i]++;
 								eX[i]>pX?eX[i]--:eX[i]++;
 							}
-
+							
 							drawPlayer(eX[i], eY[i], 20);
 						}
 					}
@@ -418,7 +394,7 @@ int main(void)
 		else
 		{
 			touch_read();
-
+			
 			if (menu == 1)
 			{
 				if (mapLoad)
@@ -426,15 +402,15 @@ int main(void)
 					drawMap(250, 0, &mapLoad);
 					drawMenu(menu);
 				}
-
-				// Play
+				
+				// Play 
 				// x : 1950 - 2300
 				// y : 800 - 1670
-
+				
 				if (flagTouch && flagReset == 1)
 				{
 					flagTouch = 0;
-
+					
 					if (touch_x>=1650 && touch_x<=2500
 							&& touch_y>=700 && touch_y<=1700)
 					{
@@ -442,7 +418,7 @@ int main(void)
 						menu = 2;
 						flagReset = 0;
 					}
-
+					
 					if (touch_x>=2500 && touch_x<=3400
 							&& touch_y>=700 && touch_y<=1700)
 					{
@@ -459,11 +435,11 @@ int main(void)
 					drawMap(250, 0, &mapLoad);
 					drawMenu(menu);
 				}
-
+				
 				if (flagTouch && flagReset == 1)
 				{
 					flagTouch = 0;
-
+					
 					// Save 3
 					if (touch_x >= 1900 && touch_x <= 3200
 							&& touch_y >= 2650 && touch_y <= 3500)
@@ -473,16 +449,15 @@ int main(void)
 						numSave = 2;
 						if (check_save(2))
 						{
-							load_save(numSave, data);
- 							// Chargement de la sauvegarde "data"
-							filldown_save(data, numSave, &life, &mapX, &mapY, &pX, &pY, 0);
+							i2c_eeprom_read(numSave*20,data,20);
+							filldown_save(data, numSave, &life, &mapX, &mapY, &pX, &pY, 0); // Chargement de la sauvegarde "data"
 						}
 						else
 						{
 							reset(&mapX, &mapY, &pX, &pY);
 						}
 					}
-
+					
 					// Save 1
 					if (touch_x >= 1900 && touch_x <= 3200
 							&& touch_y >= 1650 && touch_y <= 2650)
@@ -492,16 +467,15 @@ int main(void)
 						numSave = 1;
 						if (check_save(1))
 						{
-							load_save(numSave, data);
-							// Chargement de la sauvegarde "data"
-							filldown_save(data, numSave, &life, &mapX, &mapY, &pX, &pY, 0);
+							i2c_eeprom_read(numSave*20, data,20);
+							filldown_save(data, numSave, &life, &mapX, &mapY, &pX, &pY, 0); // Chargement de la sauvegarde "data"
 						}
 						else
 						{
 							reset(&mapX, &mapY, &pX, &pY);
 						}
 					}
-
+					
 					// Save 0
 					if (touch_x >= 1900 && touch_x <= 3200
 							&& touch_y >= 650 && touch_y <= 1650)
@@ -511,9 +485,8 @@ int main(void)
 						numSave = 0;
 						if (check_save(0))
 						{
-							load_save(numSave, data);
-							// Chargement de la sauvegarde "data"
-							filldown_save(data, numSave, &life, &mapX, &mapY, &pX, &pY, 0);
+							i2c_eeprom_read(numSave*20,data,20);
+							filldown_save(data, numSave, &life, &mapX, &mapY, &pX, &pY, 0); // Chargement de la sauvegarde "data"
 						}
 						else
 						{
@@ -529,11 +502,11 @@ int main(void)
 					drawMap(250, 0, &mapLoad);
 					drawMenu(menu);
 				}
-
+				
 				if (flagTouch && flagReset == 1)
 				{
 					flagTouch = 0;
-
+					
 					// Tutorial
 					if (touch_x>=1650 && touch_x<=2500
 							&& touch_y>=700 && touch_y<=2200)
@@ -542,7 +515,7 @@ int main(void)
 						menu = 3;
 						flagReset = 0;
 					}
-
+					
 					// Story
 					if (touch_x>=2500 && touch_x<=3400
 							&& touch_y>=700 && touch_y<=1700)
@@ -551,7 +524,7 @@ int main(void)
 						menu = 4;
 						flagReset = 0;
 					}
-
+					
 					// Back
 					if (touch_x>=2500 && touch_x<=3400
 							&& touch_y>=2200 && touch_y<=3800)
